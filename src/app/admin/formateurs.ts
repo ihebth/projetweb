6a3316a89f1159fd1e555ce87a4966d932ff3a80
ihebth <iheb.thlithi@gmail.com>
2021-01-30 14:59:37 +0100

    firstcommit

---
+++ src/app/admin/formateurs.ts
@@ -0,0 +1,29 @@
+import { Formateur } from './formateur';
+export const FORMATEURITEMS: Formateur[] = [
+    {
+     id: 1,
+     nom: "iheb",
+     prenom: "thlithi",
+     Adresse: "sokra",
+     Email: "test",
+     Domaine: "informatique"
+    },
+    {
+        id: 2,
+        nom: "anis ",
+        prenom: "mimouni",
+        Adresse: "manouba",
+        Email: "test",
+        Domaine: "informatique"
+       },
+       {
+        id: 3,
+        nom: "maher",
+        prenom: "srasra",
+        Adresse: "gabes",
+        Email: "test",
+        Domaine: "informatique"
+       },
+       
+
+];
---
+++ src/app/admin/participants.ts
@@ -0,0 +1,26 @@
+import { Participant } from './participant';
+export const PARTICIPANTSITEMS: Participant[] = [
+{
+     
+          id: 1,
+          nom: "iheb",
+          prenom: "thlithi",
+          adresse: "sokra",
+          email: "test",
+          societe:"",
+          poste: "informatique"
+         },
+         {
+             id: 2,
+             nom: "anis ",
+             prenom: "mimouni",
+             adresse: "manouba",
+             email: "test",
+             societe:"",
+             poste: "informatique"
+             
+            },
+            
+            
+     
+     ];
